d585f98f73167c2ce7209752b918af41a7544326
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-12 15:41:58 +0300

    Aligned site logo to the rest of the content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,10 +44,10 @@ const Header = () => {
   }
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}>
-        <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-          <a href="/" className="w-[12rem] xl:mr-8">
-              <img src={datawise} loading="lazy" alt="Datawise" width={65} height={20}/>
+    <div className={`fixed top-0 left-0 lg:left-20 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}>
+        <div className="flex items-center px-5 lg:px-15 xl:px-20 max-lg:py-4">
+          <a href="/" className="w-[12rem] xl:mr-12">
+              <img className="lg:ml-20" src={datawise} loading="lazy" alt="Datawise" width={65} height={20}/>
           </a>
 
           <nav className={`${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
