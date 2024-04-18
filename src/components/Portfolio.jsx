fbc608de507c4aa0e1c35780190298314c69fe5f
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 13:36:07 +0300

    Modified portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,11 +6,8 @@ const Portfolio = () => {
   return (
     <Section id="portfolio" crosses>
         <div className="container lg:flex">
-            <div className="max-w-[25rem]">
+            <div className="max-w-[80rem]">
                 <h2 className="h2 mb-4 md:mb-8">Explore our showcase of exceptional projects</h2>
-            </div>
-            <div className="lg:ml-auto xl:w-[38rem] mt-4">
-            <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">{projectText}</p>
             </div>
         </div>
         <PortfolioSection />
