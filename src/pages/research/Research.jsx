dde5c298731742c9158c19b820eafb9039d9fa0d
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 17:47:52 +0300

    Modified research page

--- src/pages/research/Research.jsx
+++ src/pages/research/Research.jsx
@@ -7,7 +7,7 @@ const Research = () => {
       
       <div className="container lg:flex mt-10 mb-10">
             <div className="max-w-[80rem]">
-                <h2 className="h2 mb-4 md:mb-8">Explore our showcase of exceptional projects</h2>
+                <h2 className="h2 mb-4 md:mb-8">Current research projects</h2>
             </div>
         </div>
         <PortfolioSection />
