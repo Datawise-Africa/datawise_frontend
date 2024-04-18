95b97cd32c5d04e0a1778f2d5f2f2bf5bf8d4bc6
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 16:05:51 +0300

    Modified what we do page

--- src/pages/company/WhatWeDo.jsx
+++ src/pages/company/WhatWeDo.jsx
@@ -1,9 +1,15 @@
 import Section from "../../components/Section"
+import ServiceDetail from "../../components/ServiceDetail"
 
 const WhatWeDo = () => {
   return (
-    <Section className="h-96 flex items-center justify-center">
-        <h1>What We Do Coming Soon</h1>
+    <Section className="mt-10 items-center justify-center">
+        <div className="container flex flex-col mt-10 mb-10">
+            <div className="max-w-[80rem]">
+                <h2 className="h2 mb-4 md:mb-8">We conduct research in several thematic areas and emerging technologies</h2>
+            </div>
+        </div>
+        <ServiceDetail />
     </Section>
   )
 }
