78236192c9e1dee0a7c232e62f23563b4fd57782
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 12:59:08 +0300

    Removed image code in Hero file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,37 +29,7 @@ const Hero = () => {
                     </p>
 
                 </div>
-
-                {/* <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
-                    <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
-                        <div className="relative bg-n-15 rounded-[1rem]">
-                            <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
-
-                            <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/410]">
-                                <img src={datawise_hero} loading="lazy" alt="hero" className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]" width={1024} height={1800}/>
-
-                                <ScrollParallax isAbsolutelyPositioned>
-                                    <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
-                                        {heroIcons.map((icon, index) => (
-                                            <li className="p-5" key={index}>
-                                                <img src={icon} loading="lazy" alt="icon" width={24} height={25} />
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </ScrollParallax>
-                            </div>
-                        </div>
-
-                        <Gradient />
-                    </div>
-                    <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-                        <img src={heroBackground} loading="lazy" alt="hero-background" className="w-full" width={1440} height={1800}/>
-                    </div>
-
-                    <BackgroundCircles />
-                </div> */}
             </div>
-            {/* <BottomLine /> */}
         </Section>
     )
 }
