74bb26f789b7d17350a160383db8156606ff9874
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 17:58:22 +0300

    Modified portfolio detail section and removed unwanted images

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -13,11 +13,10 @@ const PortfolioSection = () => {
                 >
                     <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
                         <div className="flex items-center mt-auto">
-                            <img src={item.logo} alt={item.title} loading='lazy'/>
+                            <img src={item.logo} alt={item.title} width={200} height={200} loading='lazy'/>
                         </div>
                         <h5 className='h5 mb-5 mt-5'>{item.title}</h5>
                     </div>
-                    {item.light && <GradientLight />}
                 </div>
             ))}
         </div>
