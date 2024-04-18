a3f43b1e3cb228a19fec6cde08234dce068d8f23
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-04-18 17:45:47 +0300

    Modified our services component

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -66,7 +66,7 @@ const OurServices = () => {
               </ul>
 
               {/* <LeftCurve /> */}
-              <RightCurve />
+              {/* <RightCurve /> */}
             </div>
         </div>
       </div>
