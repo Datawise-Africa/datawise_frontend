94a1aef00dc8081251042ef955075cf3cf186748
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-05-27 18:50:37 +0300

    Modified Blog.jss in src/pages/blog/Blog

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,10 +1,48 @@
-import Section from "../../components/Section"
-const Blog = () => {
-  return (
-    <Section className="h-96 flex items-center justify-center">
-        <h1>Blog Coming Soon</h1>
-    </Section>
-  )
-}
+import { useState, useEffect } from "react";
+import Section from "../../components/HomePage/Section";
+import FeatureBlog from "./FeatureBlog";
+import AllBlogs from "./AllBlogs";
+import apiService from "../../services/apiService";
+import Loader from "./Loader";
 
-export default Blog
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await apiService.get("/blogs/");
+        setBlogs(response.data);
+      } catch (error) {
+        console.log('Failed to fetch blogs', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
+  if (isLoading) {
+    <Loader />
+  }
+
+  return (
+    <Section className="py-10 pt-10">
+      <div className="container mx-auto pt-20 md:pt-10">
+            {blogs.length > 0 ? (
+              <>
+                <FeatureBlog blogs={blogs} />
+                <AllBlogs blogs={blogs}/>
+              </>
+            ) : (
+              <div className="text-center">
+                <p>Blogs Coming Soon</p>
+              </div>
+            )}
+      </div>
+    </Section>
+  );
+};
+
+export default Blog;
