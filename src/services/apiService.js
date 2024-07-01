702eb4e0bd7a35c26e8488094fbee4eddddf35f1
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-07-01 19:32:24 +0300

    Updated src/services/apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,5 @@
 import { REACT_PUBLIC_API_HOST } from "../constants";
+import { getAccessToken } from "../lib/auth/actions"
 
 const apiService = {
     post: async function (url, data) {
@@ -34,6 +35,31 @@ const apiService = {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
+                }
+            })
+            .then(response => response.json())
+            .then((json) => {
+                resolve(json);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+        })
+    },
+
+    getProtected: async function (url) {
+        console.log('get', url);
+        const accessToken = getAccessToken();
+        console.log('get', accessToken);
+
+        return new Promise((resolve, reject) => {
+            let full_url = `${REACT_PUBLIC_API_HOST}${url}`
+            fetch(full_url, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `JWT ${accessToken}`
                 }
             })
             .then(response => response.json())
