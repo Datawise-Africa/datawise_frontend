cf7700b2508c19e56905f20e2e5346ef54ae9b05
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-10-17 13:59:34 +0300

    Modified Modal component to be reusable

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -1,7 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
 
-const Modal = ({label, content, isOpen, close }) => {
-    const [showModal, setShowModal] = useState(isOpen);
+import React from 'react'
+
+const Modal = ({label, content, isOpen, close}) => {
+    const [showModal, setShowModal] = useState(isOpen)
 
     useEffect(() => {
         setShowModal(isOpen);
@@ -20,28 +22,28 @@ const Modal = ({label, content, isOpen, close }) => {
     }
 
     return (
-        <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/60">
+        <div className="flex items-center justify-center fixed inset-0 z-50 bg-black/90">
             <div className="relative w-[90%] md:w-[80%] lg:w-[700px] my-6 mx-auto h-auto">
-                <div className={`translate duration-600 h-full ${showModal ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-10'}`}>
-                    <div className="w-full h-auto rounded-xl relative flex flex-col bg-white">
-                        <header className="h-[60px] flex items-center p-6 rounded-t justify-center relative border-b">
-                            <div onClick={handleClose} className="p-3 absolute left-3 hover:bg-gray-300 rounded-full cursor-pointer">
+            <div className={`translate duration-600 h-full ${showModal ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-10'}`}>
+                <div className="w-full h-auto rounded-xl relative flex flex-col bg-n-8">
+                    <header className="h-[60px] flex items-center p-6 rounded-t justify-center relative">
+                        <h2 className="text-lg font-bold">{label}</h2>
+                        <div onClick={handleClose} className="p-3 absolute right-3 hover:bg-n-2 rounded-full cursor-pointer">
                             <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                             </svg>
-                            </div>
+                        </div>
+                    </header>
 
-                            <h2 className="text-lg font-bold">{label}</h2>
-                        </header>
-
-                        <section className="p-6">
-                            {content}
-                        </section>
-                    </div>
+                    <section className="p-6">
+                        {content}
+                    </section>
                 </div>
+            </div>
             </div>
         </div>
     )
 }
 
-export default Modal;
+export default Modal
+
