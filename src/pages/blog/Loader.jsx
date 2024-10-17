c4761f131ea570eaae58f8441589092dba6ee8e3
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-10-17 14:01:41 +0300

    Modified blog loader

--- src/pages/blog/Loader.jsx
+++ src/pages/blog/Loader.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-        <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-            <span className="visually-hidden">Loading...</span>
+    <div className="mt-20 max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: 12 }, (_, index) => (
+        <div key={index} className="mt-10 p-4 space-y-4 animate-pulse h-64">
+          <div className="h-4 bg-n-13 rounded"></div>
+          <div className="space-y-2">
+            <div className="h-10 bg-n-13 rounded w-5/6"></div>
+            <div className="h-10 bg-n-13 rounded w-4/6"></div>
+          </div>
         </div>
+      ))}
     </div>
   )
 }
