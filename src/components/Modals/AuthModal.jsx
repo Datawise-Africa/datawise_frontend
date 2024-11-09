539d26520fdf0069e1dfb93fc953e0e57eeb8670
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-09 22:30:08 +0300

    Modified AuthModal

--- src/components/Modals/AuthModal.jsx
+++ src/components/Modals/AuthModal.jsx
@@ -59,7 +59,7 @@ const AuthModal = ({navUrl}) => {
         
 
         if (response.access_token) {
-            handleLogin(response.id, response.user_role, response.access_token, response.refresh_token);
+            handleLogin(response.id, response.first_name, response.last_name, response.user_role, response.access_token, response.refresh_token);
             authModal.close()
             if (navUrl) {
                 navigate(navUrl)
@@ -101,34 +101,50 @@ const AuthModal = ({navUrl}) => {
     const content = (
         <>
             {isSignUp ? (
-                <form action={submitLogin} className='space-y-8'>
-                    <input 
-                        onChange={(e) => setFirstName(e.target.value)}
-                        placeholder='Your first name'
-                        type="text"
-                        className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                    />
+                <form action={submitLogin} className='space-y-4'>
+                    <div className='space-y-2'>
+                        <h5>Hi, Welcome to Datalab</h5>
+                        <p>Create an account or <span className='underline cursor-pointer hover:underline' onClick={() => setIsSignUp(!isSignUp)}>login to existing account</span></p>
+                    </div>
+                    <div className='pb-4'>
+                        <label htmlFor="first_name">First name</label>
+                        <input 
+                            onChange={(e) => setFirstName(e.target.value)}
+                            placeholder='Your first name'
+                            type="text"
+                            className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
-                    <input 
-                        onChange={(e) => setLastName(e.target.value)}
-                        placeholder='Your last name'
-                        type="text"
-                        className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                    />
+                    <div className='pb-4'>
+                        <label htmlFor="last_name">Last name</label>
+                        <input 
+                            onChange={(e) => setLastName(e.target.value)}
+                            placeholder='Your last name'
+                            type="text"
+                            className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
-                    <input 
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder='Your email address'
-                        type="email"
-                        className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                    />
+                    <div className='pb-4'>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='Your email address'
+                            type="email"
+                            className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
-                    <input 
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder='Your password'
-                        type="password"
-                        className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                    />
+                    <div className='pb-4'>
+                    <label htmlFor="password">Password</label>
+                        <input 
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder='Your password'
+                            type="password"
+                            className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
                     {errors.map((error, index) => {
                         return (
@@ -141,23 +157,32 @@ const AuthModal = ({navUrl}) => {
 
 
                     <CustomButton label="Sign up" onClick={submitSignUp} /> 
-                    <p className='flex gap-2 justify-center'>Already have an account? <span className='text-n-15 cursor-pointer hover:underline' onClick={() => setIsSignUp(!isSignUp)}> Sign in</span></p>
                 </form>
             ) : (
                 <form action={submitLogin} className='space-y-8'>
-                <input 
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder='Your email address'
-                    type="email"
-                    className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                />
+                    <div className='space-y-2'>
+                        <h5>Hi, Welcome to Datalab</h5>
+                        <p>Sign in to your account or <span className='underline cursor-pointer hover:underline' onClick={() => setIsSignUp(!isSignUp)}>create a new account</span></p>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='Your email address'
+                            type="email"
+                            className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
-                <input 
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder='Your password'
-                    type="password"
-                    className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
-                />
+                    <div>
+                    <label htmlFor="password">Password</label>
+                        <input 
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder='Your password'
+                        type="password"
+                        className='w-full px-4 h-[54px] border border-gray-300 rounded-xl'
+                        />
+                    </div>
 
                 {errors.map((error, index) => {
                     return (
@@ -168,9 +193,14 @@ const AuthModal = ({navUrl}) => {
                 })}
 
                 <CustomButton label="Sign in" onClick={submitLogin} /> 
-                <p className='flex gap-2 justify-center'>Don't have an account? <span className='text-n-15 cursor-pointer hover:underline' onClick={() => setIsSignUp(!isSignUp)}> Sign up</span></p>
-            </form>
+                </form>
             )}
+
+            <div className="flex items-center pt-4 pb-2">
+                <hr className="flex-grow border-t" />
+                <span className="px-2">OR</span>
+                <hr className="flex-grow border-t" />
+            </div>
             
             <div className='mt-4'>
                 <GoogleLoginButton 
