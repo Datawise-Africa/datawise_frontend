be6ce423527a88ec7daeb1b4b673bf210a69db2c
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-09 22:37:16 +0300

    Created SingleDataModal component

---
+++ src/pages/data/SingleDataModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from "../../components/Modals/Modal";
+
+const SingleDataModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleDataModal
