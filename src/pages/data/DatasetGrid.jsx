b123dfc7cce33ee7c40f032960cc307fd52ac096
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-18 20:02:21 +0300

    Modified DatasetGrid jsx in data dir

--- src/pages/data/DatasetGrid.jsx
+++ src/pages/data/DatasetGrid.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import DatasetCard from './DatasetCard';
 
-const DatasetGrid = ({ datasets }) => {
+const DatasetGrid = ({ datasets, handleSingleDataModal, handleDownloadDataClick }) => {
   return (
     <div className='md:col-span-3 lg:col-span-3 lg:mt-4'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
         {datasets.map((dataset, index) => (
-            <DatasetCard key={index} dataset={dataset} />
+            <DatasetCard key={index} dataset={dataset} handleSingleDataModal={handleSingleDataModal} handleDownloadDataClick={handleDownloadDataClick}/>
         ))}
       </div>
     </div>
