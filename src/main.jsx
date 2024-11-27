a99836f822dfdb343b7673bee64828ba8e3002ea
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-27 15:57:03 +0300

    Updated main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { PositionProvider } from "./storage/PositionProvider"
 import App from './App.jsx'
 import './index.css'
 
@@ -8,8 +9,10 @@ const clientId = '264082792754-qro0613m02oel96nardi6bf5dboruei4.apps.googleuserc
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId={clientId}>
+        <PositionProvider>
+          <App />
+        </PositionProvider>
+      </GoogleOAuthProvider>
   </React.StrictMode>,
 )
