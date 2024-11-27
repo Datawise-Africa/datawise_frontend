806af47b35e922424a1c4e94a4281e9d2e5316b0
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-27 15:56:28 +0300

    Updated app wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 // import Router from "./router/router";
-
-import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { RouterProvider} from "react-router-dom";
 import router from "./router";
 
 const App = () => {
+  
 
-  return (
-    <RouterProvider 
-      router={router}
-    />
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
- 
+export default App;
