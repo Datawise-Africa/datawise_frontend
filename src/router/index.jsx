7013b0b8c8ee66a879c31d9606590518929a46cd
BrianOdhiambo <brianodhiambo530@gmail.com>
2024-11-27 15:57:59 +0300

    Updated base router file

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,23 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import baseRoutes from "./base.routes";
 import Layout from "../pages/layout/Layout";
+import BaseWrapper from "../pages/layout/BaseWrapper";
 import Error404 from "../components/errors/Error404";
 import BaseErrorPage from "../components/errors/BaseErrorPage";
 
+
 const routes = [
-    {
+  {path: '/',
+    element: <BaseWrapper />,
+    children: [
+      {
         path: "/",
         errorElement: <BaseErrorPage />,
         element: <Layout />,
-        children: [
-            ...baseRoutes,
-        ]
-    },
-    {
+        children: [...baseRoutes],
+      },
+      {
         path: "*",
         element: <Error404 />,
-    }
-]
+      },
+    ],
+  },
+];
 
 const router = createBrowserRouter(routes);
 
